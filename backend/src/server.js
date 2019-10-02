a5b884d76dd1629b6bd0c2a457e7ec89bb27cc90
Fernando Zancope <fernando.zancope@yahoo.com.br>
2019-10-02 13:20:08 -0300

    Alterações

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-yd7c8.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}) 
 
 // MÉTODOS GET - Buscar informações do backend
 // POST - Criar informações do backend
 // PUT - Editar informação
 // DELETE - Apagar informação
+// req.query - Acessar query params (para filtros)
+// req.params - Acessar rout params (para edição, delete)
+// req.body - Acessar corpo da requisição
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello Fernando" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
