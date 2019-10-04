707a6afe47187c467bfb41e2c7384960d11fcecc
Fernando Zancope <fernando.zancope@yahoo.com.br>
2019-10-04 13:41:24 -0300

    Finalização Dashboard

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -22,6 +22,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-yd7c8.mongodb.net/
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
